68cbca37aa68b08c801f8ed331ddb8ff59912e66
xomrau <hle3524@gmail.com>
2020-07-10 02:19:37 +1000

    edited components

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = function() {
     return <div className="footer view">
-        <p>Made by <a href="https://github.com/xomrau">Danny</a> (@xomrau)</p>
+        <p>Made by <a className="link" href="https://github.com/xomrau">Danny</a> (@xomrau)</p>
     </div>;
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Header = function() {
     return <div className="wrapper-header">
-        <div className="header view">
+        <div className="header view pb-10 pt-10">
             <h1>Subject Line Assistant</h1>
-            <div>
+            <div className="logo">
                 <a href="https://github.com/xomrau">
                     <img src="https://user-images.githubusercontent.com/22007153/87010799-71a01700-c20a-11ea-883c-11efdf0eac39.png" alt="github logo"/>
                 </a>
