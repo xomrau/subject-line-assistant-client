806c1fc9816a611bb50ffbe5da19ea9930530645
xomrau <hle3524@gmail.com>
2020-07-10 12:23:30 +1000

    handled slencoder whens empty

--- src/logic/slEncoder.js
+++ src/logic/slEncoder.js
@@ -2,11 +2,14 @@ import utf8Encoder from './utf8Encoder';
 
 //format into ready-to-use utf-8-encoded subject line
 const slEncoder = (str) => {
-    let [encoding, subjectLine ,closing] = ['=?utf-8?Q?', utf8Encoder(str), '?='];
-    const [space, regex] = [/%20/gi, /%/gi];
-    subjectLine = subjectLine.replace(space, '_').replace(regex, '=');
-    return (encoding + subjectLine + closing);
-
+    if (str.length === 0) {
+        return;
+    } else {
+        let [encoding, subjectLine ,closing] = ['=?utf-8?Q?', utf8Encoder(str), '?='];
+        const [space, regex] = [/%20/gi, /%/gi];
+        subjectLine = subjectLine.replace(space, '_').replace(regex, '=');
+        return (encoding + subjectLine + closing);
+    }
 };
 
 export default slEncoder;
