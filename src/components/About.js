2fef76f411e2a0871f0523a515a94ee0f0c6f107
xomrau <hle3524@gmail.com>
2020-07-15 11:41:28 +1000

    edited the content of About comp

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const About = function() {
+const About = function () {
     return <div className="about view pt-20 pb-20">
         <h2 className="pb-10">About</h2>
         <p className="pb-5">A tool to encode subject line with emojis or any special characters.</p>
-        <p className="pb-5">Copy emojis from your favorite emoji site such as: <a className="link" href="https://getemoji.com/">Get Emoji</a> and paste it into the decoded string textfield.</p>
+        <p className="pb-5">Copy emojis from your favorite emoji site such as: <a className="link" href="https://getemoji.com/">Get Emoji</a> or click the emojis below and paste it into the decoded string textfield.</p>
     </div>;
 }
 
