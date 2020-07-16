76e48984221e86e03f216dae81e902d8e4febd87
xomrau <hle3524@gmail.com>
2020-07-17 01:45:43 +1000

    edited styling for emojilist

--- src/components/EmojiList.js
+++ src/components/EmojiList.js
@@ -2,14 +2,23 @@ import React from 'react';
 
 function EmojiList(props) {
     if (props.error) {
-        return(<div className="view">Error: {props.error.message}</div>);
+        return (<div id="error" className="view">Error: {props.error.message}</div>);
     } else if (!props.isLoaded) {
-        return(<div className="view">Loading Emoji List...!</div>);
+        return (<div id="loading" className="view">Loading Emoji List...!
+            <span role="img" aria-label="emoji" > &#x23f3;</span>
+            <ul className="flex row">
+                <li onClick={props.itemList}>E</li>
+                <li onClick={props.itemList}>M</li>
+                <li onClick={props.itemList}>O</li>
+                <li onClick={props.itemList}>J</li>
+                <li onClick={props.itemList}>I</li>
+            </ul>
+        </div>);
     } else {
-        return(<div className="view" >
+        return (<div className="view" >
             <ul className="flex row">
                 {props.items.map(item => (
-                    <li key={item.slug}>{item.character}</li>
+                    <li onClick={props.itemList} key={item.slug}>{item.character}</li>
                 ))}
             </ul>
         </div>);
