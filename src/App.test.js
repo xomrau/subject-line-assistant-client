df4368f38b3baa406ea3805d419f4198d3099e66
xomrau <hle3524@gmail.com>
2020-08-20 22:32:07 +1000

    added a simple test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import ReactDOM from 'react-dom'
+import { render, cleanup } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+afterEach(cleanup);
+
+// smoke test
+test('renders App components without crashes', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
 });
