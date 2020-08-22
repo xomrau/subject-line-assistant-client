9f18faf3f67dd2d385b686770df05d6ec70ee4f1
xomrau <hle3524@gmail.com>
2020-08-22 18:49:30 +1000

    added EmojiList comp to the App

--- src/App.js
+++ src/App.js
@@ -5,20 +5,25 @@ import About from './components/About';
 import Header from './components/Header';
 import Footer from './components/Footer';
 //import Search from './components/Search';
+import EmojiList from './components/EmojiList';
 import slEncoder from './logic/slEncoder';
 import copyClipboard from './logic/copyClipboard';
+import data from './assets/emojis.json';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       userInput: "",
-      value: ""
+      value: "",
+      isLoaded: false,
+      error: null,
+      items: [],
     };
     this.handleChange = this.handleChange.bind(this);
     //this.handleFilterChange = this.handleFilterChange.bind(this);
     this.handleClick = this.handleClick.bind(this);
-    //this.handleEmojiClick = this.handleEmojiClick.bind(this);
+    this.handleEmojiClick = this.handleEmojiClick.bind(this);
   }
   handleChange(e) {
     this.setState({
@@ -32,11 +37,29 @@ export default class App extends React.Component {
       value: slEncoder(state.userInput)
     }));
   }
+  handleEmojiClick(e) {
+    const item = e.target.innerText;
+    this.setState((state) => ({
+      userInput: state.userInput + item
+    }));
+    //console.log(`Button click input: ${e.target.innerText} ${typeof e.target.innerText}`);
+  }
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        items: data,
+        isLoaded: true,
+      });
+    }, 5000);
+  }
 
   render() {
     const {
       value,
-      userInput
+      userInput,
+      isLoaded,
+      items,
+      error
     } = this.state;
     return <div className="flex col">
       <Header />
@@ -45,6 +68,11 @@ export default class App extends React.Component {
         submitValue={value}
         userInput={userInput}
         copy={copyClipboard} />
+      <EmojiList
+        isLoaded={isLoaded}
+        items={items}
+        error={error}
+        itemList={this.handleEmojiClick} />
       <About />
       {/*<Search filterInput={filterInput}
         searchOnChange={this.handleFilterChange} />*/}
