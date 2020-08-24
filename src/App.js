0206c61bd8ad6ea743efdf98af8c8f5c74969b78
xomrau <hle3524@gmail.com>
2020-08-25 09:15:02 +1000

    added pagination to the app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Pagination } from 'semantic-ui-react';
+import 'semantic-ui-css/semantic.min.css';
 import './App.css';
 import Form from './components/Form';
 import About from './components/About';
@@ -13,17 +15,20 @@ import data from './assets/emojis.json';
 export default class App extends React.Component {
   constructor(props) {
     super(props);
+    this.numberOfItemPerPage = 30;
     this.state = {
       userInput: "",
       value: "",
       isLoaded: false,
       error: null,
       items: [],
+      activePage: 1
     };
     this.handleChange = this.handleChange.bind(this);
     //this.handleFilterChange = this.handleFilterChange.bind(this);
     this.handleClick = this.handleClick.bind(this);
     this.handleEmojiClick = this.handleEmojiClick.bind(this);
+    this.handlePaginationChange = this.handlePaginationChange.bind(this);
   }
   handleChange(e) {
     this.setState({
@@ -44,13 +49,17 @@ export default class App extends React.Component {
     }));
     //console.log(`Button click input: ${e.target.innerText} ${typeof e.target.innerText}`);
   }
+  handlePaginationChange(e, { activePage }) {
+    this.setState({ activePage });
+    //console.log(activePage);
+  }
   componentDidMount() {
     setTimeout(() => {
       this.setState({
         items: data,
         isLoaded: true,
       });
-    }, 5000);
+    }, 3000);
   }
 
   render() {
@@ -59,7 +68,8 @@ export default class App extends React.Component {
       userInput,
       isLoaded,
       items,
-      error
+      error,
+      activePage
     } = this.state;
     return <div className="flex col">
       <Header />
@@ -68,11 +78,21 @@ export default class App extends React.Component {
         submitValue={value}
         userInput={userInput}
         copy={copyClipboard} />
+      <Pagination activePage={activePage}
+        totalPages={Math.ceil(items.length / this.numberOfItemPerPage)}
+        onPageChange={this.handlePaginationChange}
+      />
       <EmojiList
         isLoaded={isLoaded}
         items={items}
         error={error}
-        itemList={this.handleEmojiClick} />
+        itemList={this.handleEmojiClick}
+        activePage={activePage}
+        itemPerPage={this.numberOfItemPerPage} />
+      <Pagination activePage={activePage}
+        totalPages={Math.ceil(items.length / this.numberOfItemPerPage)}
+        onPageChange={this.handlePaginationChange}
+      />
       <About />
       {/*<Search filterInput={filterInput}
         searchOnChange={this.handleFilterChange} />*/}
