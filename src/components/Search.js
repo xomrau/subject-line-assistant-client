bfae0aebeed6bc87efe5267f904f25f8092bf760
xomrau <hle3524@gmail.com>
2020-08-25 22:35:30 +1000

    added a Search component

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,1 +1,12 @@
 // Search Components
+import React from 'react';
+
+function Search(props) {
+    return (<div className="form view pt-20">
+        <form className="flex row">
+            <input type="text" id="search" value={props.input} onChange={props.filterChange} placeholder="Search emoji's name..." />
+        </form>
+    </div>);
+}
+
+export default Search;
