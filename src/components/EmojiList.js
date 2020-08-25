b5b79251342bd251670c845c8c39ac9c6ce316dc
xomrau <hle3524@gmail.com>
2020-08-25 22:34:33 +1000

    changed according to the pagination and filter

--- src/components/EmojiList.js
+++ src/components/EmojiList.js
@@ -8,17 +8,15 @@ function EmojiList(props) {
         arrayList = props.items.slice(props.itemPerPage * (props.activePage - 1), props.activePage * props.itemPerPage);
     }
     if (props.error) {
-        return (<div id="error" className="view">Error: {props.error.message}</div>);
+        return (<div id="error" className="view pb-20 pt-20">Error: {props.error.message}</div>);
     } else if (!props.isLoaded) {
-        return (<div id="loading" className="view">Loading Emoji List...!
+        return (<div id="loading" className="view pb-20 pt-20">Loading Emoji List...!
             <span role="img" aria-label="emoji"  > &#x23f3;</span>
         </div>);
     } else {
-        return (<div className="view" >
+        return (<div className="view pb-20 pt-20" >
             <ul className="flex row">
-                {arrayList.map(item => (
-                    <li onClick={props.itemList} key={item.slug}>{item.character}</li>
-                ))}
+                {arrayList.map(item => (<li onClick={props.itemList} key={item.slug}>{item.character}</li>))}
             </ul>
         </div>);
     }
